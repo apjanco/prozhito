4a46d7ed9142748183501ccbbcef92de22b9a5c8
ikhovryak <ikhovryak@haverford.edu>
2019-03-05 15:47:31 -0500

    Add All Journals Section

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('publications/', views.PublicationListView.as_view(), name='publications'),
     path('publication/<int:pk>', views.PublicationDetailView.as_view(), name='publication-detail'),
+    path('journals/', views.JournalListView.as_view(), name='journals'),
+    path('journal/<int:pk>', views.JournalDetailView.as_view(), name='journal-detail'),
 ]
